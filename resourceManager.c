e917f74d1d87e0bf141519234277f375d88df92a
Leo <hernleoj@gmail.com>
2023-03-19 18:24:18 -0400

    Update resourceManager.c

--- resourceManager.c
+++ resourceManager.c
@@ -68,8 +68,8 @@ int main(){
 
     
     for(int i = 0; i < numberOfInputs; i++){
-
         sscanf(input[i+1], "%s %d %d %d %d", activity, &taskNumber, &delay, &resourceType, &value);
+        printf("activity = %s, taskNumber = %d, delay = %d, resourceType = %d, value = %d\n", activity, taskNumber, delay, resourceType, value);
         taskIndex = taskNumber - 1;
         resourceIndex = resourceType - 1;
 
